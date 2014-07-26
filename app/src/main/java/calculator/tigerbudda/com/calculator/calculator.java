26e25a4c8d90aa6a8cdee945c8e54e15f4be9b05
TigerBuddha <Shanthan Challa>
2014-07-26 00:16:42 -0700

    Finished the entire thing, now I just have to add more things if I want.

--- app/src/main/java/calculator/tigerbudda/com/calculator/calculator.java
+++ app/src/main/java/calculator/tigerbudda/com/calculator/calculator.java
@@ -4,95 +4,99 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 
 public class calculator extends Activity {
 
-    TextView disp;
+    TextView d;
     EditText firstNumber;
     EditText secondNumber;
     String finalAnswer;
-    int operatorId;
+    Button b;
+    Spinner s;
+    int operatorId = 1;
     double numberOne;
     double numberTwo;
+    String[] operators = {"Add(+)", "Subtract(-)", "Multiply(x)", "Divide(/)"};
+    String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        //Load first number into numberOne
-
-        //Display First number
+        d=(TextView)findViewById(R.id.answerView);
         firstNumber=(EditText)findViewById(R.id.numberOne);
-        firstNumber.setText(new Double(numberOne).toString());
-
-        //Load second number into numberTwo
-
-        //Display Second number
         secondNumber=(EditText)findViewById(R.id.numberTwo);
-        secondNumber.setText(new Double(numberTwo).toString());
-        int i = 0;
+        b=(Button)findViewById(R.id.calculateButton);
+        s=(Spinner)findViewById(R.id.operationPicker);
+
+        //Display place holder in the solution area.
+        d.setText("SOLUTION");
+
+        //Setup spinner
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(calculator.this, android.R.layout.simple_spinner_item, operators);
+        s.setAdapter(adapter);
+
+        //Load first number into numberOne, operation to operatorId, second number into numberTwo
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Load numberOne
+                numberOne = Double.parseDouble(firstNumber.getText().toString());
+
+                //Load operator
+                operatorId = s.getSelectedItemPosition();
+
+                //Load numberTwo
+                numberTwo = Double.parseDouble(secondNumber.getText().toString());
+
+                //Calculation method: Operation
+                finalAnswer = Double.toString(operation(numberOne, numberTwo, operatorId));
 
 
-        //Calculation method: Operation
+                //Display Answer
+                d.setText(finalAnswer);
 
-        finalAnswer = Double.toString(operation(numberOne, numberTwo));
-
-
-
-        //Display Answer
-        disp=(TextView)findViewById(R.id.answerView);
-        disp.setText(new Double(finalAnswer).toString());
-
-
-
+            }
+        });
 
 
 
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.calculator, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    public double operation( double numberOne, double numberTwo){
+    public double operation( double numberOne, double numberTwo, int operatorId){
 
         double result = 0;
 
         switch(operatorId){
-            case 1:
+            case 0:
                 result = numberOne + numberTwo;
                 break;
-            case 2:
+            case 1:
                 result = Math.abs(numberOne - numberTwo);
                 break;
-            case 3:
+            case 2:
                 result = numberOne * numberTwo;
                 break;
-            case 4:
+            case 3:
                 //TODO operation for numberTwo = 0
-                result = numberOne * numberTwo;
+                if(numberTwo == 0){
+                    error = "Cannot Divide by 0";
+                    break;
+                }
+                result = numberOne / numberTwo;
                 break;
         }
 
